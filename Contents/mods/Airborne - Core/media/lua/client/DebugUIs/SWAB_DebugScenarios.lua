850d6fc5877074815582a9aa6e8e21345d1f7ac6
Brian Ostrander <Ostrander.Brian@gmail.com>
2023-02-28 13:29:06 -0800

    Zombies now cleaned up on debug scenarios.

--- Contents/mods/Airborne - Core/media/lua/client/DebugUIs/SWAB_DebugScenarios.lua
+++ Contents/mods/Airborne - Core/media/lua/client/DebugUIs/SWAB_DebugScenarios.lua
@@ -21,6 +21,37 @@ SWAB_DebugScenarios.scenarios = {
     },
 }
 
+function SWAB_DebugScenarios.EveryOneMinute()
+    if GameTime:getInstance() then
+        local minutes = GameTime:getInstance():getMinutes()
+        if minutes and minutes == 1 then
+            SWAB_DebugScenarios.CleanupZombies()
+        end
+    end
+end
+
+function SWAB_DebugScenarios.CleanupZombies()
+    local radius = 50
+    if isClient() then
+        SendCommandToServer(string.format("/removezombies -x %d -y %d -z %d -radius %d", getPlayer():getX(), getPlayer():getY(), getPlayer():getZ(), radius))
+        return
+    end
+    for x = (getPlayer():getX() - radius), (getPlayer():getX() + radius) do
+        for y= (getPlayer():getY() - radius), (getPlayer():getY() + radius) do
+            local square = getCell():getGridSquare(x, y, getPlayer():getZ())
+            if square then
+                for i = square:getMovingObjects():size(), 1, -1 do
+                    local zombie = square:getMovingObjects():get(i - 1)
+                    if instanceof(zombie, "IsoZombie") then
+                        zombie:removeFromWorld()
+                        zombie:removeFromSquare()
+                    end
+                end
+            end
+        end
+    end
+end
+
 function SWAB_DebugScenarios.SetSandbox()
     SandboxVars.VehicleEasyUse = true
     SandboxVars.Zombies = 5
@@ -42,9 +73,19 @@ function SWAB_DebugScenarios.OnStart()
         getPlayer():LevelPerk(Perks.Woodwork)
         getPlayer():LevelPerk(Perks.Mechanics)
     end
+
+    Events.EveryOneMinute.Add(SWAB_DebugScenarios.EveryOneMinute)
 end
 
 function SWAB_DebugScenarios.Initialize()
+
+    local defaultScenarios = {}
+
+    for scenario_key, scenario in pairs(debugScenarios) do
+        defaultScenarios[scenario_key] = scenario
+        debugScenarios[scenario_key] = nil
+    end
+
     for index, scenario in ipairs(SWAB_DebugScenarios.scenarios) do
         debugScenarios["swab_debug_scenario_"..index] = {
             name = "SWAB - "..scenario.name,
@@ -53,6 +94,10 @@ function SWAB_DebugScenarios.Initialize()
             onStart = SWAB_DebugScenarios.OnStart
         }
     end
+
+    for scenario_key, scenario in pairs(defaultScenarios) do
+        debugScenarios[scenario_key] = scenario
+    end 
 end
 
 SWAB_DebugScenarios.Initialize()
