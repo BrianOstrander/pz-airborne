0230dcb6fb8b45b9546713837610404560c4df88
Brian Ostrander <Ostrander.Brian@gmail.com>
2023-03-23 16:56:58 -0700

    Added standard filter crates.

--- Contents/mods/Airborne - Core/media/lua/server/SWAB_Recipe.lua
+++ Contents/mods/Airborne - Core/media/lua/server/SWAB_Recipe.lua
@@ -1,3 +1,17 @@
+SWAB_Recipe = SWAB_Recipe or {}
+SWAB_Recipe.OnTest = SWAB_Recipe.OnTest or {}
+SWAB_Recipe.OnCreate = SWAB_Recipe.OnCreate or {}
+
+function SWAB_Recipe.OnTest.PutStandardFiltersInBoxLarge(_item)
+    if PZMath.equal(1, _item:getUsedDelta()) then
+        -- This is an unused filter.
+        return true
+    end
+    
+    -- This must be a used filter.
+    return false
+end
+
 -- SWAB_Recipe = SWAB_Recipe or {}
 -- SWAB_Recipe.OnTest = SWAB_Recipe.OnTest or {}
 -- SWAB_Recipe.OnCreate = SWAB_Recipe.OnCreate or {}
